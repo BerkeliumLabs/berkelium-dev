d1356b20ae8834ef2029f24c66699c33a51193a9
Buddhi Kavindra <visitbudkavin@gmail.com>
2023-10-02 21:24:39 +0530

    remove vite project

--- berkelium/src/lib/berkelium.spec.ts
+++
@@ -1,7 +0,0 @@
-import { berkelium } from './berkelium';
-
-describe('berkelium', () => {
-  it('should work', () => {
-    expect(berkelium(30, 1)).toEqual(31);
-  });
-});
--- berkelium/src/lib/berkelium.ts
+++
@@ -1,5 +0,0 @@
-import * as wasm from '@berkelium/core';
-
-export function berkelium(x: number, y: number): number {
-  return wasm.add_numbers(x, y);
-}
